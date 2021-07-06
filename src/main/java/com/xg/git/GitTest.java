58597b2c90d42ff474b290b935aeab8d6a156960
gx <gx@qq.com>
2021-07-06 14:20:36 +0800

    Initial commit

---
+++ src/main/java/com/xg/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xg.git;
+
+/**
+ * @author gg
+ * @create 2021-07-06 14:18
+ */
+public class GitTest{
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
