61c5a9a580869301d39afba52190ca4c853ddc11
旭g <61476317+xg-Hello@users.noreply.github.com>
2021-07-06 14:22:53 +0800

    Update GitTest.java

--- src/main/java/com/xg/git/GitTest.java
+++ src/main/java/com/xg/git/GitTest.java
@@ -6,6 +6,8 @@ package com.xg.git;
  */
 public class GitTest{
     public static void main(String[] args) {
+        System.out.println("hello world");
         System.out.println("1111");
+        
     }
 }
